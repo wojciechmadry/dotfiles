553f403e680a8db4df655a9d6aeced28554fc36b
Wojciech Madry <madrywojciech99@gmail.com>
2024-10-29 16:15:43 +0100

    [Neovim] Add harpoon desc and unbind <C-f>
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -675,9 +675,12 @@ harpoon:setup({})
 -- basic telescope configuration
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end, { desc = "Add file to harpoon" } )
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon #1" } )
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon #2" })
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon #3" })
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon #4" })
 
+
+-- Unbind PgUp/PgDown
+vim.keymap.set("", "<C-f>", "<Nop>")
