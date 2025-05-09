d78959c23f48d2ed76b7db9d52577e35d8d93900
Wojciech Madry <madrywojciech99@gmail.com>
2025-05-10 01:08:05 +0200

    neovim: Update cmp nav
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -652,10 +652,16 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
+  mapping = {
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<Down>"] = cmp.mapping.abort(),
-    ["<Up>"] = cmp.mapping.abort(),
+    ["<Down>"] = function()
+      require('cmp').abort()
+      vim.cmd('stopinsert | +1')
+    end,
+    ["<Up>"] = function()
+      require('cmp').abort()
+      vim.cmd('stopinsert | -1')
+    end,
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
